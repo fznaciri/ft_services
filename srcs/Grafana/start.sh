3856d2ab33c371faca4f089929791c7646d92178
Fatima ezzahra Naciri ben saghir <fatimaezzahranaciri0@gmail.com>
2021-03-17 17:12:16 +0100

    grafana OK

--- srcs/Grafana/start.sh
+++ srcs/Grafana/start.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-telegraf --config /etc/telegraf/telegraf.conf
-rc-service grafana-server start
-rc-service grafana-server status
+# telegraf --config /etc/telegraf/telegraf.conf
+# rc-service grafana-server start
+# rc-service grafana-server status
+rc-service telegraf start
+addgroup -S grafana
+adduser -G grafana -S grafana
+chown -R grafana:grafana /grafana-7.4.3
+cd /grafana-7.4.3/bin
+./grafana-server web &
+su-exec grafana bin/grafana-server web
 
 cat /dev/random
